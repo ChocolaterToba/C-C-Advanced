0a62598cb5353fe0e52ee6fbbe7cedff8651207f
Mikhail Aglicheev <toba.ru@mail.ru>
2020-10-12 11:35:51 +0300

    Changed set-env.bat, so that Appveyor compiles c correctly

--- ci/appveyor/set-env.bat
+++ ci/appveyor/set-env.bat
@@ -45,6 +45,7 @@ set TARGET_CPU=x86
 set CMAKE_GENERATOR_SUFFIX=
 set OPENSSL_DIR=C:\OpenSSL-Win32
 shift
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 goto :loop
 
 :amd64
@@ -52,6 +53,8 @@ set TARGET_CPU=amd64
 set CMAKE_GENERATOR_SUFFIX= Win64
 set OPENSSL_DIR=C:\OpenSSL-Win64
 shift
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 goto :loop
 
 :finalize
