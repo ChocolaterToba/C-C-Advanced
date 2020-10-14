cccc16abe9f6e6b78a81535859cc0c9ddabf8a3a
Mikhail Aglicheev <toba.ru@mail.ru>
2020-10-14 16:16:36 +0300

    Added gcovr installation to install-linux.sh

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -8,3 +8,9 @@ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
 sudo apt-get install snapd
 sudo snap install cmake --classic
 ln -s /snap/cmake/current/bin/cmake $HOME/bin/cmake
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.8
+pip install gcovr
